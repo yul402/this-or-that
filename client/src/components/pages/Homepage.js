b34b34e6e85b06e88f505231825e1d18ebe28973
anuD <adilbagi22@gmail.com>
2023-09-28 20:41:03 -0700

    Dont show create button if user not signed in

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,15 +1,18 @@
 // vv necessary imports to link data from db vv
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { Link } from "react-router-dom";
+import Auth from '../../utils/auth';
+import { useQuery } from "@apollo/client";
 // vv imports gql data from queries.js vv
-import { QUERY_CHOICES } from '../../utils/queries';
+import { QUERY_CHOICES } from "../../utils/queries";
 
 const Homepage = () => {
   const { loading, data } = useQuery(QUERY_CHOICES, {
-    fetchPolicy: "no-cache"
+    fetchPolicy: "no-cache",
   });
-  
-  {/* Create variable for survey data from QUERY_CHOICES here */}
+
+  {
+    /* Create variable for survey data from QUERY_CHOICES here */
+  }
 
   return (
     <div className="card bg-white card-rounded w-50">
@@ -18,13 +21,15 @@ const Homepage = () => {
       </div>
       <div className="card-body m-5">
         <h2>Choose your survey:</h2>
-        {/* Survey Data / Titles here */}  
+        {/* Survey Data / Titles here */}
       </div>
       <div className="card-footer text-center m-3">
         <h2>Create your Survey now!</h2>
-        <Link to="/create">
-          <button className="btn btn-lg btn-danger">Create a Survey</button>
-        </Link>
+        {Auth.loggedIn() && (
+          <Link to="/create">
+            <button className="btn btn-lg btn-danger">Create a Survey</button>
+          </Link>
+        )}
       </div>
     </div>
   );
